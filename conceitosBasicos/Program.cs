361dee57b9c5e1392a419b5db738acae71157e6c
Emanuel Madeira <madeira_emanuel@outlook.com>
2023-05-01 11:20:18 -0300

    Adicionar arquivos de projeto.

---
+++ conceitosBasicos/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//EXERCÍCIOS COM VETORES
+/*
+ *crie um vetor para armazenar as idades de 5 pessoas e ao final imprima:
+ * 1) a pessoa mais nova
+ * 2) a pessoa mais velha
+ * 3) a media das idades
+ * 4) todas as idades impares
+ * 5) todas as idades pares
+*/
+using System.ComponentModel.DataAnnotations.Schema;
+
+byte[] idades = new byte[5];
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Informe a {i+1}ª idade");
+    idades[i] = byte.Parse(Console.ReadLine());
+}
+Console.WriteLine("As idades informadas foram: ");
+
+foreach(byte idade in idades)
+{
+    Console.WriteLine(idade);
+}
+
+
+
+
+
